26eb9e0a2bde5d0728af803888ca27797c310fc2
teamMOYA <teammoya@hotmail.com>
2018-04-11 20:21:32 +0200

    base

---
+++ vehicles/src/vehicles/vehicle.java
@@ -0,0 +1,14 @@
+package vehicles;
+
+public class vehicle {
+	private int cilindrada;
+	private int portes;
+	private int velocitat_actual = 0;
+	private final int velocitat_maxima = 120;
+
+	
+	public void accelerar(){
+			this.velocitat_actual += 10;
+	}
+	
+}
