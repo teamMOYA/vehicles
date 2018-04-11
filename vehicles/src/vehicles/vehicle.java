5fb40a4cb6461ef4cbbfa12754378711f24f1215
teamMOYA <teammoya@hotmail.com>
2018-04-11 20:53:59 +0200

    añadido funciones y atributos

--- vehicles/src/vehicles/vehicle.java
+++ vehicles/src/vehicles/vehicle.java
@@ -5,6 +5,8 @@ public class vehicle {
 	private int portes;
 	private int velocitat_actual = 0;
 	private final int velocitat_maxima = 120;
+	private final int marxes = 5;
+	private int marxaActual = 0;
 
 
 	public void accelerar(){
@@ -16,6 +18,18 @@ public class vehicle {
 	public void Desaccelerar(){
 		while(this.velocitat_actual > 0){
 			this.velocitat_actual -= 10;
+		}
+	}
+
+	public void pujaMarxa(){
+		while(this.marxaActual < marxes){
+			this.marxaActual++;
+		}
+	}
+
+	public void baixaMarxa(){
+		while(this.marxaActual > 0){
+			this.marxaActual--;
 		}
 	}
 
