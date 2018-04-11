fe25833aa5536323a090f05ff4d53387aa00a8a2
teamMOYA <teammoya@hotmail.com>
2018-04-11 20:34:14 +0200

    crear metodo desacelerar

--- vehicles/src/vehicles/vehicle.java
+++ vehicles/src/vehicles/vehicle.java
@@ -6,9 +6,18 @@ public class vehicle {
 	private int velocitat_actual = 0;
 	private final int velocitat_maxima = 120;
 
-	
+
 	public void accelerar(){
+		while(this.velocitat_actual < this.velocitat_maxima){
 			this.velocitat_actual += 10;
+		}
+
 	}
-	
+
+	public void Desaccelerar(){
+		while(this.velocitat_actual > 0){
+			this.velocitat_actual -= 10;
+		}
+	}
+
 }
